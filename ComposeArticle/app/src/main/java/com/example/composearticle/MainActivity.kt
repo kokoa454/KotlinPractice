b4639004f4a558d795ce55c14b3c31a805a2828e
Inari Goro <qqnq7um9k@gmail.com>
2025-02-26 01:31:50 +0900

    added ComposeArticle

---
+++ ComposeArticle/app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -0,0 +1,97 @@
+package com.example.composearticle
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.composearticle.ui.theme.ComposeArticleTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Surface
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeArticleTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Scaffold { paddingValues ->
+                        TopImage(
+                            title = stringResource(R.string.jetpack_compose_title),
+                            topMessage = stringResource(R.string.jetpack_compose_topMessage),
+                            bottomMessage = stringResource(R.string.jetback_compose_bottomMessage),
+                            modifier = Modifier.padding(paddingValues)
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TopImage(title: String, topMessage: String, bottomMessage: String, modifier: Modifier = Modifier){
+    val image = painterResource(R.drawable.bg_compose_background)
+    Column(modifier = modifier) {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.FillWidth
+        )
+        DescText(
+            title = title,
+            topMessage = topMessage,
+            bottomMessage = bottomMessage
+        )
+    }
+}
+
+@Composable
+fun DescText(title: String, topMessage: String ,bottomMessage: String){
+    Column {
+        Text(
+            text = title,
+            fontSize = 24.sp,
+            modifier = Modifier.padding(16.dp)
+        )
+        Text(
+            text = topMessage,
+            modifier = Modifier.padding(horizontal = 16.dp),
+            textAlign = TextAlign.Justify
+        )
+        Text(
+            text = bottomMessage,
+            modifier = Modifier.padding(16.dp),
+            textAlign = TextAlign.Justify
+        )
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun ArticlePreview(){
+    ComposeArticleTheme {
+        Scaffold { paddingValues ->
+            TopImage(
+                title = "Jetpack Compose tutorial",
+                topMessage = "Jetpack Compose is a modern toolkit for building native Android UI. Compose simplifies and accelerates UI development on Android with less code, powerful tools, and intuitive Kotlin APIs.",
+                bottomMessage = "In this tutorial, you build a simple UI component with declarative functions. You call Compose functions to say what elements you want and the Compose compiler does the rest. Compose is built around Composable functions. These functions let you define your app\\'s UI programmatically because they let you describe how it should look and provide data dependencies, rather than focus on the process of the UI\\'s construction, such as initializing an element and then attaching it to a parent. To create a Composable function, you add the @Composable annotation to the function name.",
+                modifier = Modifier.padding(paddingValues)
+            )
+        }
+    }
+}
