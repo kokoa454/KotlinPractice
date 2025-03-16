9b1ad42f1e794107012c25d3f2143de838ebd550
Inari Goro <qqnq7um9k@gmail.com>
2025-03-16 20:26:43 +0900

    added Task3

---
+++ ClassAndCollection/Task3/Task3.kt
@@ -0,0 +1,19 @@
+data class Event(
+    val title: String,
+    val description: String? = null,
+    val daypart: Daypart, 
+    val durationInMinutes: Int
+)
+
+enum class Daypart {
+    MORNING, AFTERNOON, EVENING
+}
+
+fun main(){
+    Event(title = "Wake up", description = "Time to get up", daypart = Daypart.MORNING, durationInMinutes = 0)
+    Event(title = "Eat breakfast", daypart = Daypart.MORNING, durationInMinutes = 15)
+    Event(title = "Learn about Kotlin", daypart = Daypart.AFTERNOON, durationInMinutes = 30)
+    Event(title = "Practice Compose", daypart = Daypart.AFTERNOON, durationInMinutes = 60)
+    Event(title = "Watch latest DevBytes video", daypart = Daypart.AFTERNOON, durationInMinutes = 10)
+    Event(title = "Check out latest Android Jetpack library", daypart = Daypart.EVENING, durationInMinutes = 45)
+}
