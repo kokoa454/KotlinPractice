102f2744e92989c33f6d9b48197302da07b92452
Inari Goro <qqnq7um9k@gmail.com>
2025-03-16 20:45:23 +0900

    added Task4

---
+++ ClassAndCollection/Task4/Task4.kt
@@ -0,0 +1,28 @@
+class Event(
+    val title: String,
+    val description: String? = null,
+    val daypart: Daypart, 
+    val durationInMinutes: Int
+)
+
+enum class Daypart {
+    MORNING, AFTERNOON, EVENING
+}
+
+fun main(){
+    val event1 = Event(title = "Wake up", description = "Time to get up", daypart = Daypart.MORNING, durationInMinutes = 0)
+    val event2 = Event(title = "Eat breakfast", daypart = Daypart.MORNING, durationInMinutes = 15)
+    val event3 = Event(title = "Learn about Kotlin", daypart = Daypart.AFTERNOON, durationInMinutes = 30)
+    val event4 = Event(title = "Practice Compose", daypart = Daypart.AFTERNOON, durationInMinutes = 60)
+    val event5 = Event(title = "Watch latest DevBytes video", daypart = Daypart.AFTERNOON, durationInMinutes = 10)
+    val event6 = Event(title = "Check out latest Android Jetpack library", daypart = Daypart.EVENING, durationInMinutes = 45)
+
+    val events = mutableListOf<Event>(event1, event2, event3, event4, event5, event6)
+    printShortTasks(events)
+}
+
+fun printShortTasks(events: List<Event>) {
+    val shortEvent = events.filter { it.durationInMinutes < 30 }
+    
+    println("You have ${shortEvent.size} short events")
+}
