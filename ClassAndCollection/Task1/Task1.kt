4baa53cc74acce4d5c95c0e6342146e1b9f8df89
Inari Goro <qqnq7um9k@gmail.com>
2025-03-16 20:25:47 +0900

    updated Task1

--- ClassAndCollection/Task1/Task1.kt
+++ ClassAndCollection/Task1/Task1.kt
@@ -1,4 +1,8 @@
-class Event(title: String, description: String? = null, daypart: String, durationInMinutes: Int)
+class Event(
+    val title: String,
+    val description: String? = null,
+    val daypart: String,
+    val durationInMinutes: Int)
 
 fun main(){
     Event(title="Study Kotlin", description="Commit to studying Kotlin at least 15 minutes per day.", daypart="Evening", durationInMinutes=15)
