18a86dcc944e1e4ad8775770b897df03010cca3a
Inari Goro <qqnq7um9k@gmail.com>
2025-04-09 14:48:44 +0900

    updated Home screen composable (with bug)

--- MyCity/app/src/main/java/com/example/mycity/ui/MyCityHomeScreen.kt
+++ MyCity/app/src/main/java/com/example/mycity/ui/MyCityHomeScreen.kt
@@ -1,2 +1,76 @@
 package com.example.mycity.ui
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.mediumTopAppBarColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.mycity.R
+import com.example.mycity.data.Category
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppTopBar(
+    canNavigateBack: Boolean = false,
+    modifier: Modifier = Modifier
+){
+    TopAppBar(
+        title = { Text(stringResource(R.string.app_name)) },
+        colors = mediumTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer
+        ),
+        navigationIcon = {
+            if (canNavigateBack){
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = stringResource(R.string.back_button)
+                    )
+                }
+            }
+        },
+        modifier = modifier
+    )
+}
+
+@Composable
+fun AppHomeListItem(
+    category: Category,
+    onItemClick: (Category) -> Unit,
+    modifier: Modifier = Modifier
+){
+    Card(
+        modifier = modifier,
+        onClick = { onItemClick(category) }
+    ){
+        Column(){
+
+        }
+    }
+}
+
+@Preview
+@Composable
+fun AppTopBarPreview(){
+    AppTopBar()
+}
+
+@Preview
+@Composable
+fun AppHomeListItemPreview(){
+    AppHomeListItem(
+        category = Category(LocalContext.current.resources.getStringArray(R.array.category_names)[0], R.drawable.image1),
+        onItemClick = {}
+    )
+}
