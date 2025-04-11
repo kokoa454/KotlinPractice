1acac05bd16fe950621c4b92fb6cb7e4706b1638
Inari Goro <qqnq7um9k@gmail.com>
2025-04-11 13:37:57 +0900

    re-initialized MyCity to fix render bugs

--- MyCity/app/src/main/java/com/example/mycity/MainActivity.kt
+++ MyCity/app/src/main/java/com/example/mycity/MainActivity.kt
@@ -4,7 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.example.mycity.ui.MyCityApp
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.example.mycity.ui.theme.MyCityTheme
 
 class MainActivity : ComponentActivity() {
@@ -13,8 +19,29 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyCityTheme {
-                MyCityApp()
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
             }
         }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyCityTheme {
+        Greeting("Android")
     }
 }
--- MyCity/app/src/main/java/com/example/mycity/model/Place.kt
+++
@@ -1,9 +0,0 @@
-package com.example.mycity.model
-
-import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
-
-data class Place(
-    val name: String,
-    @DrawableRes val image: Int,
-)
