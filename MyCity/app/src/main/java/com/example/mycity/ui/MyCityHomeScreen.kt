3e8a97b0659f5b9a83f20b92d456c3193c70a7c9
Inari Goro <qqnq7um9k@gmail.com>
2025-04-14 12:00:56 +0900

    updated CategoryListItem UI

--- MyCity/app/src/main/java/com/example/mycity/ui/MyCityHomeScreen.kt
+++ MyCity/app/src/main/java/com/example/mycity/ui/MyCityHomeScreen.kt
@@ -1,12 +1,16 @@
 package com.example.mycity.ui
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
@@ -21,6 +25,7 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -78,16 +83,34 @@ fun CategoryListItem(
             containerColor = MaterialTheme.colorScheme.surfaceVariant
         )
     ) {
-        Column(
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally,
+        Box(
+            contentAlignment = Alignment.Center,
             modifier = Modifier.fillMaxSize()
         ){
             Text(
                 text = category.name,
                 style = MaterialTheme.typography.titleLarge.copy(fontSize = 36.sp)
             )
+            Row(
+                horizontalArrangement = Arrangement.End,
+                verticalAlignment = Alignment.Bottom,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(bottom = 16.dp, end = 16.dp)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.atr_black),
+                    contentDescription = null,
+                    modifier = Modifier.size(24.dp)
+                )
+                Text(
+                    text = category.number.toString(),
+                    style = MaterialTheme.typography.titleMedium.copy(fontSize = 24.sp),
+                    modifier = Modifier.padding(start = 8.dp)
+                )
+            }
         }
+
     }
 }
 
