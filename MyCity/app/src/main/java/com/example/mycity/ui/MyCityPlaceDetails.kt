b698849cd3b477bef495380aba7ffce790db3cb4
Inari Goro <qqnq7um9k@gmail.com>
2025-04-15 12:27:16 +0900

    implemented place details UI

---
+++ MyCity/app/src/main/java/com/example/mycity/ui/MyCityPlaceDetails.kt
@@ -0,0 +1,52 @@
+package com.example.mycity.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.mycity.model.Place
+import com.example.mycity.data.PlaceList.places
+
+@Composable
+fun PlaceDetails(
+    place: Place,
+    modifier: Modifier = Modifier
+){
+    Column(
+        modifier = modifier.fillMaxWidth()
+    ){
+        Text(
+            text = place.name,
+            style = MaterialTheme.typography.titleLarge.copy(fontSize = 36.sp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(color = MaterialTheme.colorScheme.primaryContainer)
+                .padding(16.dp)
+        )
+        Text(
+            text = place.description,
+            style = MaterialTheme.typography.bodyLarge.copy(
+                fontSize = 24.sp,
+                lineHeight = 32.sp
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        )
+    }
+}
+
+@Preview
+@Composable
+fun PlaceDetailsPreview(){
+    PlaceDetails(
+        place = places.first()
+    )
+}
