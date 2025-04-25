6ec9688077e2c59993ea3823934db48c3cfd643a
Inari Goro <qqnq7um9k@gmail.com>
2025-04-25 14:50:38 +0900

    implemented Error and loading screens in cards(bug: videos are not showed sometimes)

--- Amphibians/app/src/main/java/com/example/amphibians/ui/screen/HomeScreen.kt
+++ Amphibians/app/src/main/java/com/example/amphibians/ui/screen/HomeScreen.kt
@@ -3,9 +3,7 @@ package com.example.amphibians.ui.screen
 import android.widget.FrameLayout
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -14,13 +12,14 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
@@ -44,11 +43,19 @@ fun HomeScreen(
     }
 }
 
+enum class AmphibiansCardState {
+    LOADING,
+    ERROR,
+    SUCCESS
+}
+
 @Composable
 fun AmphibiansCard(
     amphibiansPhoto: AmphibiansPhoto,
     modifier: Modifier = Modifier
 ){
+    var cardState by remember { mutableStateOf(AmphibiansCardState.LOADING) }
+
     Card(
         modifier = modifier
             .fillMaxWidth()
@@ -69,17 +76,57 @@ fun AmphibiansCard(
                     .fillMaxWidth()
                     .padding(8.dp)
             )
-            AsyncImage(
-                model = ImageRequest.Builder(context = LocalContext.current)
-                    .data(amphibiansPhoto.imgSrc)
-                    .crossfade(true)
-                    .build(),
-                contentDescription = amphibiansPhoto.name,
-                contentScale = ContentScale.Crop,
-                placeholder = painterResource(R.drawable.loading),
-                modifier = Modifier
-                    .fillMaxWidth()
-            )
+
+            when (cardState) {
+                AmphibiansCardState.LOADING -> {
+                    val context = LocalContext.current
+
+                    val exoPlayer = remember {
+                        ExoPlayer.Builder(context).build().apply {
+                            setMediaItem(MediaItem.fromUri("android.resource://${context.packageName}/${R.raw.loading}"))
+                            repeatMode = ExoPlayer.REPEAT_MODE_ALL
+                            playWhenReady = true
+                            prepare()
+                        }
+                    }
+                    AsyncImage(
+                        model = ImageRequest.Builder(context = LocalContext.current)
+                            .data(amphibiansPhoto.imgSrc)
+                            .crossfade(true)
+                            .build(),
+                        onSuccess = { cardState = AmphibiansCardState.SUCCESS},
+                        onError = { cardState = AmphibiansCardState.ERROR },
+                        contentDescription = null,
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    VideoPlayer(exoPlayer)
+                }
+                AmphibiansCardState.SUCCESS -> {
+                    AsyncImage(
+                        model = ImageRequest.Builder(LocalContext.current)
+                            .data(amphibiansPhoto.imgSrc)
+                            .crossfade(true)
+                            .build(),
+                        contentDescription = amphibiansPhoto.name,
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                }
+                AmphibiansCardState.ERROR -> {
+                    val context = LocalContext.current
+                    val exoPlayer = remember {
+                        ExoPlayer.Builder(context).build().apply {
+                            setMediaItem(MediaItem.fromUri("android.resource://${context.packageName}/${R.raw.error}"))
+                            repeatMode = ExoPlayer.REPEAT_MODE_ONE
+                            playWhenReady = true
+                            prepare()
+                        }
+                    }
+                    VideoPlayer(exoPlayer)
+                }
+            }
+
             Text(
                 text = amphibiansPhoto.description,
                 style = MaterialTheme.typography.bodyLarge,
@@ -123,16 +170,12 @@ fun LoadingScreen(
     val exoPlayer = remember{
         ExoPlayer.Builder(context).build().apply {
             setMediaItem(MediaItem.fromUri("android.resource://${context.packageName}/${R.raw.loading}"))
-            repeatMode = ExoPlayer.REPEAT_MODE_OFF
+            repeatMode = ExoPlayer.REPEAT_MODE_ALL
             playWhenReady = true
             prepare()
         }
     }
     VideoPlayer(exoPlayer = exoPlayer)
-    Text(
-        text = stringResource(R.string.loading),
-        modifier = modifier.padding(16.dp)
-    )
 }
 
 @Composable
@@ -149,10 +192,6 @@ fun ErrorScreen(
         }
     }
     VideoPlayer(exoPlayer = exoPlayer)
-    Text(
-        text = stringResource(R.string.loading_failed),
-        modifier = modifier.padding(16.dp)
-    )
 }
 
 @Composable
@@ -171,8 +210,13 @@ fun VideoPlayer(
                 )
             }
         },
-        modifier = modifier
+        modifier = modifier.fillMaxWidth()
     )
+    DisposableEffect(exoPlayer) {
+        onDispose {
+            exoPlayer.release()
+        }
+    }
 }
 
 @Preview
