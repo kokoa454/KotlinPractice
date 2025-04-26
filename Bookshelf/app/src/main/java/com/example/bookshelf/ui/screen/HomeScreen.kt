94c95b8c5c708c64d306d45819aabf008a7fa15b
Inari Goro <qqnq7um9k@gmail.com>
2025-04-26 22:13:48 +0900

    implemented BookList

--- Bookshelf/app/src/main/java/com/example/bookshelf/ui/screen/HomeScreen.kt
+++ Bookshelf/app/src/main/java/com/example/bookshelf/ui/screen/HomeScreen.kt
@@ -1,7 +1,12 @@
 package com.example.bookshelf.ui.screen
 
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
@@ -9,7 +14,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
@@ -50,5 +54,30 @@ fun BookCard(
             contentScale = ContentScale.Crop,
             modifier = Modifier.fillMaxWidth()
         )
+    }
+}
+
+@Composable
+fun BookList(
+    books: List<BookPhoto>,
+    modifier: Modifier = Modifier
+){
+    LazyVerticalGrid(
+        columns = GridCells.Adaptive(150.dp),
+        modifier = modifier.fillMaxWidth(),
+        contentPadding = PaddingValues(4.dp)
+    ) {
+        items(
+            items = books,
+            key = { book -> book.id }
+        ){
+            book -> BookCard(
+                bookPhoto = book,
+                modifier = modifier
+                    .padding(4.dp)
+                    .fillMaxWidth()
+                    .aspectRatio(1.5f)
+            )
+        }
     }
 }
